c27c2bb5aefd14dc497762569a78284ee10fc923
Jkiten <wjdrlduf13@naver.com>
2021-11-13 16:04:52 +0900

    edit table

--- src/main/java/com/kiyeol/repository/UserRepository.java
+++ src/main/java/com/kiyeol/repository/UserRepository.java
@@ -13,11 +13,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     //로그인하기
-    @Query(value = "SELECT * FROM USER WHERE id like %:id% AND pw like %:pw%",nativeQuery = true)
+    @Query(value = "SELECT * FROM user WHERE id like %:id% AND pw like %:pw%",nativeQuery = true)
     Optional<User> login(@Param("id")String id,@Param("pw")String pw);
 
     //이름으로 유저찾기
-    @Query(value = "SELECT * FROM USER WHERE name like %:name%",nativeQuery = true)
+    @Query(value = "SELECT * FROM user WHERE name like %:name%",nativeQuery = true)
     Optional<User> searchUser(@Param("name")String name);
 
 }
